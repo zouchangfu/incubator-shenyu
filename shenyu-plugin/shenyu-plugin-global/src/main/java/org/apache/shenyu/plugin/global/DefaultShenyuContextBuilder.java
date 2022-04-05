f7fa7d3f18158943ecabb89323a856c63218e96c
zouchangfu <zcf13543354971@163.com>
2022-04-05 11:22:23 +0800

    增加GlobalPlugin插件注释

--- shenyu-plugin/shenyu-plugin-global/src/main/java/org/apache/shenyu/plugin/global/DefaultShenyuContextBuilder.java
+++ shenyu-plugin/shenyu-plugin-global/src/main/java/org/apache/shenyu/plugin/global/DefaultShenyuContextBuilder.java
@@ -62,6 +62,9 @@ public class DefaultShenyuContextBuilder implements ShenyuContextBuilder {
     public ShenyuContext build(final ServerWebExchange exchange) {
         ServerHttpRequest request = exchange.getRequest();
         String path = request.getURI().getPath();
+        // 根据路径获取元数据
+        // 通过路径匹配到的元数据来断定当前请求的类型，如果没有获取到元数据类型的话，默认后续是通过divide插件构建请求地址
+        // 这个元数据在可以在管理端中的 元数据管理 中进行配置
         MetaData metaData = MetaDataCache.getInstance().obtain(path);
         HttpHeaders headers = request.getHeaders();
         String upgrade = headers.getFirst(UPGRADE);
@@ -70,11 +73,29 @@ public class DefaultShenyuContextBuilder implements ShenyuContextBuilder {
             exchange.getAttributes().put(Constants.META_DATA, metaData);
             rpcType = metaData.getRpcType();
         } else if (StringUtils.isNotEmpty(upgrade) && RpcTypeEnum.WEB_SOCKET.getName().equals(upgrade)) {
+            // websocket 类型
             rpcType = RpcTypeEnum.WEB_SOCKET.getName();
         } else {
+            // 前面条件都不符合的话，而且没有携带rpc_type，说明当前请求是普通的Http注册到网关的
+            // 如果没有设置到请求头没有携带rpc_type 类型的话，默认就是 http 类型，后续走divide插件
             String rpcTypeParam = request.getHeaders().getFirst(RPC_TYPE);
             rpcType = StringUtils.isEmpty(rpcTypeParam) ? RpcTypeEnum.HTTP.getName() : rpcTypeParam;
         }
+
+        // 根据rpc类型,构建不同的全局上下文Context
+        // 这些rpc类是在哪里被初始化的呢？
+        // 在插件的starter中，这些类被注入到了Spring容器中，例如：
+        // 在SpringCloudPluginConfiguration 可以看到 SpringCloudShenyuContextDecorator 被注入到Spring容器里面了
+
+        // 这些类是怎么被放到 decoratorMap中的呢？
+        // 在 GlobalPluginConfiguration 类中，一下代码构建了rpc类的装饰Map集合
+        // “Map<String, ShenyuContextDecorator> decoratorMap = decoratorList.stream().collect(Collectors.toMap(ShenyuContextDecorator::rpcType, e -> e));”
+
+        /*
+           总体逻辑：
+           1、获取rpc类型，这些rpc类型在元数据中保存
+           2、根据不同的rpc类型构建不同的全局上下文context
+         */
         return decoratorMap.get(rpcType).decorator(buildDefaultContext(request), metaData);
     }
 
