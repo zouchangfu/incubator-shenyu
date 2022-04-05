c3c4f0546309f81a9c875abe0b9c1afc7c862ac6
zouchangfu <zcf13543354971@163.com>
2022-04-05 10:51:16 +0800

    增加SpringCloud插件注释

--- shenyu-plugin/shenyu-plugin-springcloud/src/main/java/org/apache/shenyu/plugin/springcloud/SpringCloudPlugin.java
+++ shenyu-plugin/shenyu-plugin-springcloud/src/main/java/org/apache/shenyu/plugin/springcloud/SpringCloudPlugin.java
@@ -62,24 +62,35 @@ public class SpringCloudPlugin extends AbstractShenyuPlugin {
     @Override
     protected Mono<Void> doExecute(final ServerWebExchange exchange, final ShenyuPluginChain chain, final SelectorData selector,
                                    final RuleData rule) {
+        /*
+          SpringCloudPlugin 使用负载均衡器根据处理对象的服务id获取服务实例并重建 URI，
+          据此 URI 生成真实的 URL，最后设置最终的 url 和超时时间交由插件链下游进行处理。
+         */
         if (Objects.isNull(rule)) {
             return Mono.empty();
         }
         final ShenyuContext shenyuContext = exchange.getAttribute(Constants.CONTEXT);
         assert shenyuContext != null;
+        // 获取在管理端配置选择器数据
         final SpringCloudSelectorHandle springCloudSelectorHandle = SpringCloudPluginDataHandler.SELECTOR_CACHED.get().obtainHandle(selector.getId());
+        // 获取在管理端配置的规则数据
         final SpringCloudRuleHandle ruleHandle = SpringCloudPluginDataHandler.RULE_CACHED.get().obtainHandle(CacheKeyUtils.INST.getKey(rule));
+        // 获取serviceId，也就是你springCloud项目配置的 spring.application.name
         String serviceId = springCloudSelectorHandle.getServiceId();
         if (StringUtils.isBlank(serviceId)) {
             Object error = ShenyuResultWrap.error(exchange,
                     ShenyuResultEnum.CANNOT_CONFIG_SPRINGCLOUD_SERVICEID, null);
             return WebFluxResultUtils.result(exchange, error);
         }
+        // 获取客户端的来源IP，根据IP做负载均衡
+        // 这里负载均衡是借助了 SpringCloud 的 Ribbon 组件来实现的，ShenYu内部不需要自己写实现逻辑
         String ip = Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress();
         LoadBalanceKey loadBalanceKey = new LoadBalanceKey(ip, selector.getId(), ruleHandle.getLoadBalance());
         ServiceInstance serviceInstance;
         try {
             LoadBalanceKeyHolder.setLoadBalanceKey(loadBalanceKey);
+            // 获取负责均衡实例
+            // 注意：在 shenyu-bootstrap的application.yml配置文件中需要把网关注入到Eureka中，并且还需要开启负责均衡
             serviceInstance = loadBalancer.choose(serviceId);
         } finally {
             LoadBalanceKeyHolder.resetLoadBalanceKey();
@@ -89,10 +100,19 @@ public class SpringCloudPlugin extends AbstractShenyuPlugin {
                     ShenyuResultEnum.SPRINGCLOUD_SERVICEID_IS_ERROR, null);
             return WebFluxResultUtils.result(exchange, error);
         }
+
+        // 构建目标服务的URI地址
         URI uri = loadBalancer.reconstructURI(serviceInstance, URI.create(shenyuContext.getRealUrl()));
+        // 构建真正的请求地址，最后请求会通过httpclient插件来发起远程调用
         setDomain(uri, exchange);
         //set time out.
         exchange.getAttributes().put(Constants.HTTP_TIME_OUT, ruleHandle.getTimeout());
+        /*
+           总体思路：
+           1、请求匹配到规则
+           2、根据服务Id，通过负载均衡组件获取实例
+           3、构建真实的请求路径
+         */
         return chain.execute(exchange);
     }
 
